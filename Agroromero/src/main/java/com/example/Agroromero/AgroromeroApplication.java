ce104b932cecad1c6ef06f4201616ed00e83341c
Cristian Gonzalez <cristian-saul-66@hotmail.com>
2022-09-02 21:38:33 -0500

    modi

--- Agroromero/src/main/java/com/example/Agroromero/AgroromeroApplication.java
+++ Agroromero/src/main/java/com/example/Agroromero/AgroromeroApplication.java
@@ -3,11 +3,11 @@ package com.example.Agroromero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class AgroromeroApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(AgroromeroApplication.class, args);
+		SpringApplication.run(AgroromeroApplication.class, args);
 	}
 
 }
