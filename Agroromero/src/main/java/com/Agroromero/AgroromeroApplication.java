2570a6b1094a04d382de5e7566fe5e7fe23cda9b
Cristian Gonzalez <cristian-saul-66@hotmail.com>
2022-09-06 23:52:10 -0500

    reset

--- Agroromero/src/main/java/com/Agroromero/AgroromeroApplication.java
+++ Agroromero/src/main/java/com/Agroromero/AgroromeroApplication.java
@@ -1,13 +1,13 @@
-package com.example.Agroromero;
+package com.Agroromero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class AgroromeroApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(AgroromeroApplication.class, args);
+		SpringApplication.run(AgroromeroApplication.class, args);
 	}
 
 }
